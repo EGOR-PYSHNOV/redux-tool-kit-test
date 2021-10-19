3ef659cff762a70637cce5ab34be05111d0d6949
Egor Pyshnov <egor475@yahoo.com>
2021-10-20 01:44:12 +0300

    add mutation and add json-server

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import { IPost } from '../models/IPost'
 import { postApi } from '../services/PostService'
 import { PostItem } from './PostItem'
 
 export const PostContainer = () => {
-    const { data: posts } = postApi.useFetchAllPostsQuery(5)
+    const { data: posts } = postApi.useFetchAllPostsQuery(15)
+    const [createPost, {}] = postApi.useCreatePostMutation()
+    const [updatePost, {}] = postApi.useUpdatePostMutation()
+    const [deletePost, {}] = postApi.useDeletePostMutation()
+
+    const handleCreatePost = async () => {
+        const title = prompt()
+        await createPost({ title, body: title } as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
     return (
         <div>
             <div className='posts__list'>
                 {posts?.map((post) => (
-                    <PostItem post={post} key={post.id} />
+                    <PostItem
+                        remove={handleRemove}
+                        update={handleUpdate}
+                        post={post}
+                        key={post.id}
+                    />
                 ))}
+            </div>
+            <div>
+                <button onClick={handleCreatePost}>Add new post</button>
             </div>
         </div>
     )
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -3,14 +3,25 @@ import { IPost } from '../models/IPost'
 
 interface IPostItem {
     post: IPost
+    remove: (post: IPost) => void
+    update: (post: IPost) => void
 }
 
-export const PostItem: React.FC<IPostItem> = ({ post }) => {
+export const PostItem: React.FC<IPostItem> = ({ post, remove, update }) => {
+    const handleRemove = (event: React.MouseEvent) => {
+        event.stopPropagation()
+        remove(post)
+    }
+
+    const handleUpdate = (event: React.MouseEvent) => {
+        const title = prompt() || ''
+        update({ ...post, title })
+    }
     return (
-        <div className='post'>
+        <div className='post' onClick={handleUpdate}>
             {post.id}. {post.title}
             <div>
-                <button>Delete</button>
+                <button onClick={handleRemove}>Delete</button>
             </div>
         </div>
     )
